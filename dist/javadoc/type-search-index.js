64bad785581ce1c4a0a31cc69884273ec87ce8de
Pere_Roca <73195339+peroivi@users.noreply.github.com>
2022-11-20 21:13:49 +0100

    javadoc

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"edu.epsevg.prop.lab.c4","l":"Aleatori"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.epsevg.prop.lab.c4","l":"citroënC4"},{"p":"edu.epsevg.prop.lab.c4","l":"Juga2"},{"p":"edu.epsevg.prop.lab.c4","l":"Manual"},{"p":"edu.epsevg.prop.lab.c4","l":"NewJFrame"},{"p":"edu.epsevg.prop.lab.c4","l":"Prova"}];updateSearchResults();
